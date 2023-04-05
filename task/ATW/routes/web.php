d5e7f21f2f40d4e6775c19405a4de6e747516d7d
youmna3 <youmna54@gmail.com>
2023-04-05 23:38:30 +0200

    edit dash board

--- task/ATW/routes/web.php
+++ task/ATW/routes/web.php
@@ -17,9 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('admin');
-});
+})->middleware(['auth', 'verified'])->name('admin');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
